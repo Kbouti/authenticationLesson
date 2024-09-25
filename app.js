8519eb733beaa742b165a6767d21828f6e790634
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-25 11:13:45 -0700

    Attempt to submit form results to database

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ let database = process.env.DEV_DATABASE;
 let password = process.env.DEV_PASSWORD;
 let port = process.env.DEV_PORT;
 
-console.log(`checking process.env variable user: ${user}`);
-
 const pool = new Pool({
   // add your configuration
   host,
@@ -33,5 +31,24 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("signUpForm"));
+
+
+app.post("/sign-up", async (req, res, next) => {
+console.log(`post route reached`)
+
+
+// Why do we do the weird thing with the $1, $2.... I feel like that's tripping us up. 
+    try {
+    console.log(`About to run query`);
+      await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+        req.body.username,
+        req.body.password,
+      ]);
+      res.redirect("/");
+    } catch(err) {
+      return next(err);
+    }
+  });
+  
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
