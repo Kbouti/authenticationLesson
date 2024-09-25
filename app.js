056e26a35771727c3858a62948c36872c8017060
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-25 10:56:15 -0700

    Create environment variables for db

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-
 const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 
+require("dotenv").config();
 
-let host = "localhost";
-let user = "kbouti";
-let database = "kbouti";
-let password;
-let port;
+let host = process.env.DEV_HOST;
+let user = process.env.DEV_USER;
+let database = process.env.DEV_DATABASE;
+let password = process.env.DEV_PASSWORD;
+let port = process.env.DEV_PORT;
 
-
+console.log(`checking process.env variable user: ${user}`);
 
 const pool = new Pool({
   // add your configuration
+  host,
+  user,
+  database,
+  password,
+  port,
 });
 
 const app = express();
