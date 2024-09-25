396c75d690d488debe667dcabedde7d38ecd9e04
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-25 15:01:27 -0700

    Add troubleshooting logs

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ let database = process.env.DEV_DATABASE;
 let password = process.env.DEV_PASSWORD;
 let port = process.env.DEV_PORT;
 
+console.log(`host: ${host}`);
+console.log(`user: ${user}`);
+console.log(`database: ${database}`);
+console.log(`password: ${password}`);
+console.log(`port: ${port}`);
+
 const pool = new Pool({
   // add your configuration
   host,
@@ -32,23 +38,29 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("signUpForm"));
 
-
 app.post("/sign-up", async (req, res, next) => {
-console.log(`post route reached`)
+  console.log(`post route reached`);
 
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`username: ${username}`);
+  console.log(`password: ${password}`);
 
-// Why do we do the weird thing with the $1, $2.... I feel like that's tripping us up. 
-    try {
+  const testQuery = `select * from users;`;
+
+  // Why do we do the weird thing with the $1, $2.... I feel like that's tripping us up.
+  // Nahh Doesn't seem like that's it. We may not yet  be successfully connected to the pool
+  try {
     console.log(`About to run query`);
-      await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-        req.body.username,
-        req.body.password,
-      ]);
-      res.redirect("/");
-    } catch(err) {
-      return next(err);
-    }
-  });
-  
+    // await pool.query(testQuery);
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      "Stan",
+      "Marsh",
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
