3e7946c3d977dc0101e1806ff58d137812fe3426
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-26 11:03:41 -0700

    Copy passport functions

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const pool = new Pool({
   host,
   user,
   database,
-  port: 5432
-//   Hard coding default port for pool. A separate port from .env is used for express app.
+  port: 5432,
+  //   Hard coding default port for pool. A separate port from .env is used for express app.
 });
 
 const app = express();
@@ -27,6 +27,8 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`App is listening on port: ${port}`));
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/sign-up", (req, res) => res.render("signUpForm"));
@@ -46,4 +48,41 @@ app.post("/sign-up", async (req, res, next) => {
   }
 });
 
-app.listen(port, () => console.log(`App is listening on port: ${port}`));
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
